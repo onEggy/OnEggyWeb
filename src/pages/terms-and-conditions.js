cf08391488d09d63b692cbdd3ec9ccf78cf2b3d6
Aakash Sharma <33760157+RedAakash@users.noreply.github.com>
2024-06-14 12:00:35 +0530

    Update terms-and-conditions.js

--- src/pages/terms-and-conditions.js
+++ src/pages/terms-and-conditions.js
@@ -1,8 +1,8 @@
 // pages/terms-and-conditions.js
 import React from "react";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
-import Sidebar from "../components/sidebar";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Sidebar from "./components/sidebar";
 import seoData from "../../public/data/seo-data.json";
 import { NextSeo } from "next-seo";
 
