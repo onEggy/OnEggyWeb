759ec966ce055f6a3871c264aaaec4bbfc58141c
Aakash Sharma <33760157+RedAakash@users.noreply.github.com>
2024-10-23 18:49:52 +0530

    Update technologyStack.js

--- src/pages/service/technologyStack.js
+++ src/pages/service/technologyStack.js
@@ -46,7 +46,7 @@ const technologyStack = () => {
       <Headline title={title} desc={desc} descCss={' md:mt-8'} />
       <Marquee pauseOnHover={false} speed={30} pauseOnClick={false} gradient={true} gradientColor={"white"} autoFill={true}>
         {technologyStackImages.map((image, index) => (
-          <img key={index} src={image} alt={`Tech Stack Image ${index + 1}`} className="mx-2 mt-16 sm:mx-3 md:mx-4 lg:mx-6" />
+          <img key={index} src={image} alt={`Tech Stack Image ${index + 1}`} className="mt-16 mx-[2.5rem] sm:mx-[3rem] md:mx-[3.5rem] lg:mx-[4rem]" />
         ))}
       </Marquee>
     </div>
