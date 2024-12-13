f824557a5f2e7ded4df31702edd293fe471704ad
ishqDehlvi <inzemamhaq05@gmail.com>
2024-12-13 20:40:14 +0530

    newpage added

---
+++ blogsSitemapCreate.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+//changes
+
+// Load your JSON config file
+const config = require('./public/AllBlogs/index.json');
+
+const baseUrl = 'https://www.oneggy.com/blogs/';
+
+// Function to generate sitemap content
+const generateSitemapXml = (config) => {
+  const pagesXml = config.map(page =>
+    `<url>
+      <loc>${baseUrl}${page.slug}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>`
+  ).join('');
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pagesXml}
+</urlset>`;
+};
+
+// Write sitemap.xml to the public directory
+fs.writeFileSync(
+  path.resolve(__dirname, 'public/blogs-sitemap.xml'),
+  generateSitemapXml(config)
+);
+
+console.log('Sitemap generated successfully!');
